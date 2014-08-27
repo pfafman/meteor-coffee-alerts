72461ec1d9805346561258b70f07c6cc511d8f8d
Timothy Pfafman <tim@pfafman.com>
2014-08-26 18:41:10 -0700

    for 0.9

--- package.js
+++ package.js
@@ -26,6 +26,14 @@ Package.on_use(function(api, where) {
 
 Package.on_test(function(api) {
     api.use("pfafman:coffee-alerts", 'client'); 
-    api.use(['tinytest', 'test-helpers', 'coffeescript'], 'client');
+    api.use([
+        'tinytest', 
+        'test-helpers', 
+        'coffeescript', 
+        'minimongo', 
+        'mongo-livedata', 
+        'templating',
+        'iron:router'
+    ], 'client');
     api.add_files('alerts_tests.coffee', 'client'); 
 });
